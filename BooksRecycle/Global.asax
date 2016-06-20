513a6f709c671dd9b137befc7089b8ed19b7c27e
lsc <lsc@LSC-P50>
2016-06-20 09:38:30 +0800

    Add project files.

---
+++ BooksRecycle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BooksRecycle.MvcApplication" Language="C#" %>
---
+++ BooksRecycle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BooksRecycle.Startup))]
+namespace BooksRecycle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
